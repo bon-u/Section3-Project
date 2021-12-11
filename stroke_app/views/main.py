546f443157f09b513a10171bc3a0072e2f16bc12
unknown <qhsdn5350@gmail.com>
2021-12-11 22:01:43 +0900

    project

--- stroke_app/views/main.py
+++ stroke_app/views/main.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
-@main.route('/')
+@main.route('/',methods=['POST', 'GET'])
 def index():
     return render_template('index.html'), 200
