2a79dbb0a810885e24cf096b1d72d810fc5adb02
unknown <qhsdn5350@gmail.com>
2021-12-13 10:59:12 +0900

    project

--- stroke_app/views/main.py
+++ stroke_app/views/main.py
@@ -1,22 +1,7 @@
-from flask import Blueprint, render_template,request
-from . import pred
+from flask import Blueprint, render_template
 
 main = Blueprint('main', __name__)
 
 @main.route('/',methods=['POST', 'GET'])
 def index():
     return render_template('index.html'), 200
-
-@main.route('/result')
-def index():
-    req=request.form
-    data=list(req.values())
-    try:
-        ans=pred.Predict(data)
-        if int(ans)==1:
-            re='위험합니다'
-        else:
-            re='좋습니다 건강 유지해주세요 ^^'
-    except ValueError:
-            re='다시 처음부터 진행해 주십시오'
-    return render_template("result.html",ans=re), 200
